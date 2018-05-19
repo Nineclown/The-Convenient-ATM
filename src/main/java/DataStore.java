df02980020592280f627344f3420fd25076d07a1
hyeon1018 <hyeon1018@live.com>
2018-05-19 12:52:22 +0900

    Create DataStore

---
+++ src/main/java/DataStore.java
@@ -0,0 +1,24 @@
+import com.google.gson.Gson;
+
+public class DataStore{
+    //Test
+    public static void main(String[] args) {
+        
+    }
+
+    public Account loadAccountData(Enum bank, String accountNo){
+        Account acct=null;
+
+        return acct;
+    }
+
+    public void saveAccountData(Account account){
+
+    }
+
+    public User loadUserData(String userID){
+        User user=null;
+
+        return user;
+    }
+}
