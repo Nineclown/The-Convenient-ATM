8fcad2d45b7398bb29d733c9a29683a692d66762
Jinho Jeong <jjinho0203@gmail.com>
2018-05-19 15:57:39 +0900

    Add System Class

---
+++ src/main/java/System.java
@@ -0,0 +1,73 @@
+import java.util.Date;
+
+public class System {
+    private int cashAmount;
+    private int selectedCardNumber;
+
+    public void selectFunction(FunctionType function) {
+
+    }
+
+    public void enterAccountInfo(Bank bank, String accountNo) {
+
+    }
+
+    public void enterBill(int[] billAmount) {
+
+    }
+
+    public void enterBillAsDollar(int[] billAmount) {
+
+    }
+
+    public void enterPassword(int password) {
+
+    }
+
+    public void enterBillAmountToWithdraw(int cashAmount) {
+
+    }
+
+    public void enterBillAmountToWithdrawAsDollar(int cashAmount) {
+
+    }
+
+    public int[] calcBillAmount(int cashAmount) {
+        return null;
+    }
+
+    public double getCurrency() {
+        return 0.0; 
+    }
+
+    public void enterCashAmountToTransfer(int cashAmount) {
+
+    }
+
+    public void enterTotalCashAmountToGet(int cashAmount) {
+
+    }
+
+    public void enterNumberOfUsers(int userNumber) {
+
+    }
+
+    public void enterPeriodToQuery(Date start, Date end) {
+
+    }
+
+    public void enterUserId(String userId) {
+
+    }
+
+    public void selectCard(String cardNumber) {
+
+    }
+
+    public void requestStopCard(String cardNumber) {
+
+    }
+
+    public static void main(String [] args) throws Exception {
+    }
+}
