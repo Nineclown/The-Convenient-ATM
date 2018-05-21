f737f503021493936c0f4856cc6c5ce2a604359f
swp1234 <swp5516@naver.com>
2018-05-21 19:14:37 +0900

    Withdraw class 2개
    completed

--- src/main/java/ATMSystem.java
+++ src/main/java/ATMSystem.java
@@ -20,16 +20,18 @@ public class ATMSystem {
     private Transaction toTransaction;
     private ArrayList<Transaction> splitToTransaction;
 
+
     public int getCashAmount() {
         return this.cashAmount;
     }
 
-    public void setCashAmount(int value) { this.cashAmount = value; }
+    public void setCashAmount(int value) {
+        this.cashAmount = value;
+    }
 
     public ATMSystem() {
         this.cashAmount = 0;
         this.selectedCardNumber = 0;
-
         this.admins = new ArrayList<Admin>();
     }
 
@@ -80,7 +82,7 @@ public class ATMSystem {
     public void enterBill(int[] billAmount) throws InvalidBillException {
         int total = 0;
 
-        if ( billAmount.length != BillType.wonSize ) {
+        if (billAmount.length != BillType.wonSize) {
             throw new InvalidBillException();
         }
 
@@ -95,7 +97,7 @@ public class ATMSystem {
     public void enterBillAsDollar(int[] billAmount) throws InvalidBillException {
         int totalDollar = 0;
 
-        if ( billAmount.length != BillType.dollarSize ) {
+        if (billAmount.length != BillType.dollarSize) {
             throw new InvalidBillException();
         }
 
@@ -105,7 +107,7 @@ public class ATMSystem {
         totalDollar += BillType.count(BillType.DollarFifty, billAmount[3]);
         totalDollar += BillType.count(BillType.DollarHundred, billAmount[4]);
 
-        this.cashAmount += (int)(this.getCurrency() * totalDollar);
+        this.cashAmount += (int) (this.getCurrency() * totalDollar);
     }
 
     public void enterPassword(int password) {
@@ -113,32 +115,64 @@ public class ATMSystem {
     }
 
     public void enterBillAmountToWithdraw(int cashAmount) {
+        this.cashAmount = cashAmount;
+        calcBillAmount((this.cashAmount), "WON");
+
 
     }
 
     public void enterBillAmountToWithdrawAsDollar(int cashAmount) {
+        this.cashAmount = cashAmount;
+        calcBillAmount(this.cashAmount, "Dollar");
 
     }
 
-    public int[] calcBillAmount(int cashAmount) {
-        return null;
+    public int[] calcBillAmount(int cashAmount, String cashType) {
+
+        int[] dollarBill;
+        int[] wonBill;
+        int[] billType;
+        int length;
+
+        if (cashType.equals(("WON"))) {
+            wonBill = new int[2];
+            billType = new int[]{50000, 10000};
+            length = billType.length;
+            for (int i = 0; i < length; i++) {
+                wonBill[length - (i + 1)] = cashAmount / wonBill[i];
+                cashAmount -= billType[i] * wonBill[length - (i + 1)];
+            }
+        } else if (cashType.equals("Dollar")) {
+            dollarBill = new int[4];
+            billType = new int[]{
+                100, 50, 20, 10
+            };
+            length = billType.length;
+            for (int i = 0; i < length; i++) {
+                dollarBill[length - (i + 1)] = cashAmount / billType[i];
+                cashAmount -= billType[i] * dollarBill[length - (i + 1)];
+            }
+        } else {
+            return null;
+        }
+        return billType;
     }
 
     public double getCurrency() {
         double currency = 0;
 
         String url = "https://free.currencyconverterapi.com/api/v5/convert?q=USD_KRW&compact=ultra";
-        try{
+        try {
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("GET");
             con.getResponseCode();
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
-            if((inputLine = in.readLine()) != null){
-                currency = Double.parseDouble(inputLine.substring(inputLine.indexOf(':')+1, inputLine.length()-1));
+            if ((inputLine = in.readLine()) != null) {
+                currency = Double.parseDouble(inputLine.substring(inputLine.indexOf(':') + 1, inputLine.length() - 1));
             }
-        }catch(Exception e){
+        } catch (Exception e) {
 
         }
         return currency;
@@ -173,7 +207,7 @@ public class ATMSystem {
     }
 
     public void askRenewCard(boolean answer) {
-        if ( answer ) {
+        if (answer) {
 
         } else {
 
@@ -181,7 +215,7 @@ public class ATMSystem {
     }
 
     public void requestRenewCard(boolean answer) {
-        if ( answer ) {
+        if (answer) {
 
         } else {
 
@@ -197,18 +231,18 @@ public class ATMSystem {
     }
 
     public String createAdminId() {
-        if ( this.admins.size() == 0 ) {
+        if (this.admins.size() == 0) {
             return "0";
         }
         return MessageFormat.format("{0}",
-            Integer.parseInt(this.admins.get(this.admins.size() - 1).getId()+1));
+            Integer.parseInt(this.admins.get(this.admins.size() - 1).getId() + 1));
     }
 
     public int enterATMBalance(int[] billAmount) throws InvalidBillException {
         int balance = 0;
         double currency = this.getCurrency();
 
-        if ( billAmount.length != (BillType.wonSize + BillType.dollarSize) ) {
+        if (billAmount.length != (BillType.wonSize + BillType.dollarSize)) {
             throw new InvalidBillException();
         }
 
