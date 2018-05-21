4277f3073a0840549a8ed283cec139dea62c2781
Jinho Jeong <jjinho0203@gmail.com>
2018-05-21 16:29:12 +0900

    Resolve SyntaxError in AccountTest

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,59 +1,61 @@
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Steps;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SerenityRunner.class)
 public class AccountTest {
+    public DataStore dataStore;
 
-    @Steps
-    Account account;
+    @Before
+    public void initDataStore() {
+        this.dataStore = new DataStore();
+    }
 
     @Managed(driver = "chrome")
     WebDriver browser;
 
     @Test
     public void shouldGetCorrectAccount() {
-    Datastore ds = new Datastore();
-    Account acc = ds.loadAccountData("HANA","123456789012345");
-    assertEquals(acc.getBalance(),1004200);
-    assertEquals(acc.getPassword(),5555);
+        Account account = this.dataStore.loadAccountData(Bank.HANA,"123456789012345");
+        assertEquals(account.getBalance(),1004200);
+        assertEquals(account.getPassword(),5555);
     }
 
     @Test
     public void shouldGetCorrectBalance()
     {
-        Datastore ds = new Datastore();
-        Account acc = ds.loadAccountData("HANA","123456789012345");
-        acc.changeBalance(5000);
-        assertEquals(acc.getBalance(),10555);
+        Account account = this.dataStore.loadAccountData(Bank.HANA,"123456789012345");
+        account.changeBalance(5000);
+        assertEquals(account.getBalance(),10555);
     }
 
     @Test
-    public void shouldGetCorrectTranasactions()
+    public void shouldGetCorrectTransactions()
     {
-        Datastore ds = new Datastore();
-        Account acc = ds.loadAccountData("HANA","123456789012345");
-        Transaction [] transactions = acc.getTransactions(new Date(2018,5,19,12,0,0),new Date(2018,5,20,0,0,0));
-        assertEquals(transactions[1].getAmount(),100);
-        assertEquals(transactions[2].getTime(),new Date(2018,5,19,15,16,27 );
+        Account account = this.dataStore.loadAccountData(Bank.HANA,"123456789012345");
+        Transaction [] transactions = account.getTransactions(new Date(2018,5,19,12,0,0),new Date(2018,5,20,0,0,0));
+        assertEquals(transactions[1].getAmount(),1200);
+        assertEquals(transactions[2].getTime(),new Date(2018,5,19,15,16,27 ));
         assertEquals(transactions.length,4);
     }
 
     @Test
     public void shouldAddTransactionCorrectly()
     {
-        Transaction tc = new Transaction("DEPOSIT");
-        tc.setAccount(new Account("KOOKMIN","15151515");
-        tc.setAmount(5100);
-        tc.setTime();
-        Account acc = ds.loadAccountData("HANA","123456789012345");
-        acc.addTransaction(tc);
+        Transaction transaction = new Transaction(TransactionType.Deposit);
+        transaction.setAccount(new Account(Bank.KOOKMIN, "15151515141414"));
+        transaction.setAmount(5100);
+        transaction.setTime();
+        Account acc = this.dataStore.loadAccountData(Bank.HANA,"123456789012345");
+        acc.addTransaction(transaction);
         Transaction [] transactions = acc.getTransactions(new Date(2018,5,19,12,0,0),new Date(2018,5,30,0,0,0));
         assertEquals(transactions[4].getAmount(),5100);
-        )
     }
-
 }
