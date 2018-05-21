1c5a9a23d4be9fa4d743a527e010bdb8cab70910
Jinho Jeong <jjinho0203@gmail.com>
2018-05-21 19:23:52 +0900

    add test for enterAccount

--- src/test/java/ATMSystemTest.java
+++ src/test/java/ATMSystemTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import java.text.MessageFormat;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 @RunWith(SerenityRunner.class)
@@ -24,8 +25,27 @@ public class ATMSystemTest {
         system.enterBill(billAmount);
     }
 
+    @Test(expected = InvalidBillException.class)
+    public void enterBillAsDollarRaisesExceptionIfLengthIsInvalid() throws InvalidBillException, DataStoreError {
+        int [] billAmount = { 1, 2 };
+
+        system.enterBillAsDollar(billAmount);
+    }
     @Test(expected = AccountDoesNotExist.class)
     public void enterAccountInfoRaisesExceptionIfAccountDoesNotExistOnDataStore() throws AccountDoesNotExist {
         system.enterAccountInfo(Bank.WOORI, "DOESNOTEXIST");
+    }
+
+    @Test
+    public void enterAccountInfoChangesProperty()  {
+        try {
+            system.enterAccountInfo(Bank.HANA, "123456789012345");
+        } catch (AccountDoesNotExist e) {
+            fail("throw AccountDoesNotExist" + e.getMessage());
+        }
+
+        assertNotNull(system.getAccount());
+        assertEquals(system.getAccount().getBank(), Bank.HANA);
+        assertEquals(system.getAccount().getAccountNo(), "123456789012345");
     }
 }
