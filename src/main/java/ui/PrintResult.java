a8b0a91963b6062b0a0a77d78b4069dfa9550477
Jinho Jeong <jjinho0203@gmail.com>
2018-05-21 17:28:53 +0900

    Add print result form

---
+++ src/main/java/ui/PrintResult.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class PrintResult {
+    private JTextField accountNumberField;
+    private JTextField accountBalanceField;
+    private JButton confirmButton;
+
+    public PrintResult() {
+        confirmButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+    }
+}
