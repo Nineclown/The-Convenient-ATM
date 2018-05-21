3c927ee97be54aeadc57117d3e75c43e416b2b2b
swp1234 <swp5516@naver.com>
2018-05-21 16:09:02 +0900

    create AccountTest

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -15,7 +15,45 @@ public class AccountTest {
     WebDriver browser;
 
     @Test
-    public void shouldInterpretingBasedOn() {
-
+    public void shouldGetCorrectAccount() {
+    Datastore ds = new Datastore();
+    Account acc = ds.loadAccountData("HANA","123456789012345");
+    assertEquals(acc.getBalance(),1004200);
+    assertEquals(acc.getPassword(),5555);
     }
+
+    @Test
+    public void shouldGetCorrectBalance()
+    {
+        Datastore ds = new Datastore();
+        Account acc = ds.loadAccountData("HANA","123456789012345");
+        acc.changeBalance(5000);
+        assertEquals(acc.getBalance(),10555);
+    }
+
+    @Test
+    public void shouldGetCorrectTranasactions()
+    {
+        Datastore ds = new Datastore();
+        Account acc = ds.loadAccountData("HANA","123456789012345");
+        Transaction [] transactions = acc.getTransactions(new Date(2018,5,19,12,0,0),new Date(2018,5,20,0,0,0));
+        assertEquals(transactions[1].getAmount(),100);
+        assertEquals(transactions[2].getTime(),new Date(2018,5,19,15,16,27 );
+        assertEquals(transactions.length,4);
+    }
+
+    @Test
+    public void shouldAddTransactionCorrectly()
+    {
+        Transaction tc = new Transaction("DEPOSIT");
+        tc.setAccount(new Account("KOOKMIN","15151515");
+        tc.setAmount(5100);
+        tc.setTime();
+        Account acc = ds.loadAccountData("HANA","123456789012345");
+        acc.addTransaction(tc);
+        Transaction [] transactions = acc.getTransactions(new Date(2018,5,19,12,0,0),new Date(2018,5,30,0,0,0));
+        assertEquals(transactions[4].getAmount(),5100);
+        )
+    }
+
 }
