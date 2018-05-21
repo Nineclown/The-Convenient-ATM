bfa503f658f12582b3478593d6aedd5d3aa46d4c
Jinho Jeong <jjinho0203@gmail.com>
2018-05-21 17:41:27 +0900

    Add event listeners on authorize admin form

--- src/main/java/ui/AuthorizeAdmin.java
+++ src/main/java/ui/AuthorizeAdmin.java
@@ -1,6 +1,8 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class AuthorizeAdmin extends JFrame {
     private JTextField adminIdField;
@@ -8,6 +10,21 @@ public class AuthorizeAdmin extends JFrame {
     private JButton confirmButton;
     private JButton cancelButton;
     private JPanel authorizeAdminPanel;
+
+    public AuthorizeAdmin() {
+        confirmButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        cancelButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+    }
 
     public JPanel getPanel() {
         return this.authorizeAdminPanel;
