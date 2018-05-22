302555744162f5f9084a7e8f373a7fc7f262e4ef
Jinho Jeong <jjinho0203@gmail.com>
2018-05-22 17:17:34 +0900

    More reactive ui in EnterAdminInfo

--- src/main/java/com/swad/cppatm/ui/EnterAdminInfo.java
+++ src/main/java/com/swad/cppatm/ui/EnterAdminInfo.java
@@ -8,6 +8,8 @@ import com.swad.cppatm.exceptions.InvalidAdminException;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -18,33 +20,45 @@ public class EnterAdminInfo extends JFrame {
     private JPasswordField adminPwField;
     private JTextField adminContactField;
 
+    public void next(JFrame parentFrame, ATMSystem system) {
+        String password = new String(adminPwField.getPassword());
+        String contact = adminContactField.getText();
+
+        try {
+            system.enterAdminInfo(password, contact);
+        }catch(DataStoreError er){
+            JOptionPane.showMessageDialog(parentFrame, "Failed to add Admin", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        Admin newAdmin = system.getAdmins()[system.getAdmins().length-1];
+        JOptionPane.showMessageDialog(parentFrame, "Admin added\nYour AdminId : " + newAdmin.getId(), "Info", JOptionPane.INFORMATION_MESSAGE);
+
+        parentFrame.setContentPane(new AdminSelectFunction(parentFrame, system).getPanel());
+        parentFrame.invalidate();
+        parentFrame.validate();
+    }
+
     public EnterAdminInfo(final JFrame parentFrame, final ATMSystem system) {
         confirmButton.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-                String password = new String(adminPwField.getPassword());
-                String contact = adminContactField.getText();
-
-                try {
-                    system.enterAdminInfo(password, contact);
-                }catch(DataStoreError er){
-                    JOptionPane.showMessageDialog(parentFrame, "Failed to add Admin", "Error", JOptionPane.ERROR_MESSAGE);
-                }
-                Admin newAdmin = system.getAdmins()[system.getAdmins().length-1];
-                JOptionPane.showMessageDialog(parentFrame, "Admin added\nYour AdminId : " + newAdmin.getId(), "Info", JOptionPane.INFORMATION_MESSAGE);
-
+            public void mousePressed(MouseEvent e) {
+                next(parentFrame, system);
+            }
+        });
+        cancelButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
                 parentFrame.setContentPane(new AdminSelectFunction(parentFrame, system).getPanel());
                 parentFrame.invalidate();
                 parentFrame.validate();
             }
         });
-        cancelButton.addMouseListener(new MouseAdapter() {
+        adminContactField.addKeyListener(new KeyAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                parentFrame.setContentPane(new AdminSelectFunction(parentFrame, system).getPanel());
-                parentFrame.invalidate();
-                parentFrame.validate();
+            public void keyPressed(KeyEvent e) {
+                switch(e.getKeyCode()) {
+                    case KeyEvent.VK_ENTER:
+                        next(parentFrame, system);
+                }
             }
         });
     }
