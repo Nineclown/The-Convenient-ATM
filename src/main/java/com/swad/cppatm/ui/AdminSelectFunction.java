7e0311b7b3a6f20cf1af16647a70d4f73b3e15a1
Jinho Jeong <jjinho0203@gmail.com>
2018-05-22 15:19:26 +0900

    resolve bugs

--- src/main/java/com/swad/cppatm/ui/AdminSelectFunction.java
+++ src/main/java/com/swad/cppatm/ui/AdminSelectFunction.java
@@ -59,7 +59,7 @@ public class AdminSelectFunction extends JFrame {
                 } catch (NoneOfFunctionSelected ex) {
                     return;
                 }
-                parentFrame.setContentPane(new QueryATMBalance(zparentFrame, system).getPanel());
+                parentFrame.setContentPane(new QueryATMBalance(parentFrame, system).getPanel());
                 parentFrame.invalidate();
                 parentFrame.validate();
             }
