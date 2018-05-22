7e85e00ecdd3a9a3b45bb9f210b2a64701aa3308
hyeon1018 <hyeon1018@live.com>
2018-05-22 15:03:46 +0900

    implements enterAccountInfo

--- src/main/java/com/swad/cppatm/ui/RequestCardOrBankbook.java
+++ src/main/java/com/swad/cppatm/ui/RequestCardOrBankbook.java
@@ -1,6 +1,9 @@
 package com.swad.cppatm.ui;
 
 import com.swad.cppatm.application.ATMSystem;
+import com.swad.cppatm.enums.Bank;
+import com.swad.cppatm.exceptions.AccountDoesNotExist;
+import com.swad.cppatm.exceptions.DataStoreError;
 
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
@@ -9,25 +12,50 @@ import java.awt.event.MouseEvent;
 public class RequestCardOrBankbook {
     private JPanel requestCardOrBankbookPanel;
     private JTextField bankbookNumberField;
-    private JTextField cardNumberField;
     private JButton confirmButton;
     private JButton cancelButton;
+    private JRadioButton buttonHana;
+    private JRadioButton buttonKookmin;
+    private JRadioButton buttonWoori;
+    private ButtonGroup bankgroup;
 
     public RequestCardOrBankbook(final JFrame parentFrame, final ATMSystem system) {
+        bankgroup = new ButtonGroup();
+        bankgroup.add(buttonHana);
+        bankgroup.add(buttonKookmin);
+        bankgroup.add(buttonWoori);
+
         confirmButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if ( bankbookNumberField.getText().length() > 0 && cardNumberField.getText().length() > 0 ) {
-                    JOptionPane.showMessageDialog(parentFrame, "Only one of the number is accepted.", "Error", JOptionPane.ERROR_MESSAGE);
-                    bankbookNumberField.setText("");
-                    cardNumberField.setText("");
+
+                Bank bank;
+                if(buttonHana.isSelected()){
+                    bank = Bank.HANA;
+                }else if(buttonWoori.isSelected()){
+                    bank = Bank.WOORI;
+                }else if(buttonKookmin.isSelected()){
+                    bank = Bank.KOOKMIN;
+                }else{
+                    JOptionPane.showMessageDialog(parentFrame, "BankName is Invaild.", "Error", JOptionPane.ERROR_MESSAGE);
                     return;
                 }
 
-                if ( bankbookNumberField.getText().length() == 0 && cardNumberField.getText().length() == 0 ) {
+                if ( bankbookNumberField.getText().length() == 0) {
                     JOptionPane.showMessageDialog(parentFrame, "No number at all." , "Error", JOptionPane.ERROR_MESSAGE);
                     return;
                 }
+                String accountNo = bankbookNumberField.getText();
+
+                try{
+                    system.enterAccountInfo(bank, accountNo);
+                }catch(DataStoreError ex){
+                    JOptionPane.showMessageDialog(parentFrame, "Load Error", "Error", JOptionPane.ERROR_MESSAGE);
+                }catch(AccountDoesNotExist ex){
+                    JOptionPane.showMessageDialog(parentFrame, "Can't Find Account", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+
+
             }
         });
         cancelButton.addMouseListener(new MouseAdapter() {
