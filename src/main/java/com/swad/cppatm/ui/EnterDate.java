13b415916ad18a9757285a75dd17ad69c08293fe
hyeon1018 <hyeon1018@live.com>
2018-05-30 20:25:40 +0900

    change to JNumberTextField on EnterData

--- src/main/java/com/swad/cppatm/ui/EnterDate.java
+++ src/main/java/com/swad/cppatm/ui/EnterDate.java
@@ -5,6 +5,7 @@ import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.swad.cppatm.application.ATMSystem;
 import com.swad.cppatm.enums.Locale;
 import com.swad.cppatm.exceptions.AccountDoesNotExist;
+import com.swad.cppatm.ui.components.JNumberTextField;
 
 import javax.swing.*;
 import java.awt.*;
@@ -113,8 +114,10 @@ public class EnterDate {
      */
     private void $$$setupUI$$$() {
         enterDatePanel = new JPanel();
-        enterDatePanel.setLayout(new GridLayoutManager(3, 3, new Insets(0, 0, 0, 0), -1, -1));
+        enterDatePanel.setLayout(new GridLayoutManager(3, 2, new Insets(0, 0, 0, 0), -1, -1));
         enterDatePanel.setBackground(new Color(-1));
+        Font enterDatePanelFont = this.$$$getFont$$$(null, -1, 20, enterDatePanel.getFont());
+        if (enterDatePanelFont != null) enterDatePanel.setFont(enterDatePanelFont);
         startLabel = new JLabel();
         Font startLabelFont = this.$$$getFont$$$(null, -1, 20, startLabel.getFont());
         if (startLabelFont != null) startLabel.setFont(startLabelFont);
@@ -127,18 +130,10 @@ public class EnterDate {
         endLabel.setForeground(new Color(-16777216));
         endLabel.setText("끝");
         enterDatePanel.add(endLabel, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_EAST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        startDateField = new JTextField();
-        Font startDateFieldFont = this.$$$getFont$$$(null, -1, 20, startDateField.getFont());
-        if (startDateFieldFont != null) startDateField.setFont(startDateFieldFont);
-        enterDatePanel.add(startDateField, new GridConstraints(0, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        endDateField = new JTextField();
-        Font endDateFieldFont = this.$$$getFont$$$(null, -1, 20, endDateField.getFont());
-        if (endDateFieldFont != null) endDateField.setFont(endDateFieldFont);
-        enterDatePanel.add(endDateField, new GridConstraints(1, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         final JPanel panel1 = new JPanel();
         panel1.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
         panel1.setBackground(new Color(-1));
-        enterDatePanel.add(panel1, new GridConstraints(2, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        enterDatePanel.add(panel1, new GridConstraints(2, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         cancelButton = new JButton();
         cancelButton.setBackground(new Color(-10592674));
         Font cancelButtonFont = this.$$$getFont$$$(null, -1, 20, cancelButton.getFont());
@@ -153,6 +148,15 @@ public class EnterDate {
         confirmButton.setForeground(new Color(-1));
         confirmButton.setText("확인");
         panel1.add(confirmButton, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(-1, 60), null, 0, false));
+        endDateField = new JNumberTextField();
+        Font endDateFieldFont = this.$$$getFont$$$(null, -1, 20, endDateField.getFont());
+        if (endDateFieldFont != null) endDateField.setFont(endDateFieldFont);
+        endDateField.setText("");
+        enterDatePanel.add(endDateField, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        startDateField = new JNumberTextField();
+        Font startDateFieldFont = this.$$$getFont$$$(null, -1, 20, startDateField.getFont());
+        if (startDateFieldFont != null) startDateField.setFont(startDateFieldFont);
+        enterDatePanel.add(startDateField, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
     }
 
     /**
