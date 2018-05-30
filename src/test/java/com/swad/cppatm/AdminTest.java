51fa3921c3a4e95cb24dd9826d3eaa803ba9b518
hyeon1018 <hyeon1018@live.com>
2018-05-30 21:45:21 +0900

    remove unused unit test

--- src/test/java/com/swad/cppatm/AdminTest.java
+++ src/test/java/com/swad/cppatm/AdminTest.java
@@ -10,29 +10,6 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class AdminTest {
-
-    @Test(expected = InvalidAdminException.class)
-    public void shouldConstructorCheckContactsValidity() throws InvalidAdminException {
-        Admin admin = new Admin("1", "1234", "123412341234");
-        admin = new Admin("1", "1234", "010-1234-1234");
-        admin = new Admin("2", "1234", "01012341234");
-
-        assertEquals("1", admin.getId());
-        assertEquals("1234", admin.getPassword());
-        assertEquals("01012341234", admin.getContact());
-    }
-
-    @Test(expected = InvalidAdminException.class)
-    public void shouldConstructorCheckPasswordValidity() throws InvalidAdminException {
-        Admin admin = new Admin("1", "abcd", "01012341234");
-        admin = new Admin("1", "asifj9qfhe", "01012341234");
-        admin = new Admin("1", "1234", "01012341234");
-
-        assertEquals("1", admin.getId());
-        assertEquals("1234", admin.getPassword());
-        assertEquals("01012341234", admin.getContact());
-    }
-
     @Test
     public void shouldCheckAdminAccount() {
         DataStore ds = new DataStore();
